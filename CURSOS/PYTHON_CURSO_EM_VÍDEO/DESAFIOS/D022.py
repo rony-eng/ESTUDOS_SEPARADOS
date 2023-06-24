63876c9c9a64451739e58d7e9e363d3f8d91b902
Rony-Menezes <134240502+rony-eng@users.noreply.github.com>
2023-06-24 14:51:56 -0300

    Updete Desafio Python
    Resolução (incompleta) do desafio D022 do curso em vídeo - Python

--- CURSOS/PYTHON_CURSO_EM_VÍDEO/DESAFIOS/D022.py
+++ CURSOS/PYTHON_CURSO_EM_VÍDEO/DESAFIOS/D022.py
@@ -4,3 +4,12 @@
 # - O NOME COM TODAS MINÚSCULAS
 # - QUANTAS LETRAS AO TODO (SEM CONSIDERAR ESPAÇOS)
 # - QUANTAS LETRAS TEM O PRIMEIRO NOME
+
+nome = input('Insira seu nome completo: ')
+
+print('')
+print('[Letras Maiusculas:] ', nome.upper()) # letras maiusculas
+print('[Letras Minusculas:] ', nome.lower()) # letras minusculas
+print('[Cumprimento String:]', len(nome), 'letras') # Cumprimento da String
+print('[Comprimento 1° Nome:]')
+print('')
