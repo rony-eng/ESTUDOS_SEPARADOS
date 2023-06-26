4a8eb6ccccd1bcc50cc159248778fdd2f7d869a6
Rony-Menezes <134240502+rony-eng@users.noreply.github.com>
2023-06-26 15:00:07 -0300

    Update D022 Desafio Python
    Atualização, desafio D022.py conclusão da atividade.

--- CURSOS/PYTHON_CURSO_EM_VÍDEO/DESAFIOS/D022.py
+++ CURSOS/PYTHON_CURSO_EM_VÍDEO/DESAFIOS/D022.py
@@ -5,11 +5,18 @@
 # - QUANTAS LETRAS AO TODO (SEM CONSIDERAR ESPAÇOS)
 # - QUANTAS LETRAS TEM O PRIMEIRO NOME
 
-nome = input('Insira seu nome completo: ')
+print('------------------------------')
+nome = str(input('Insira seu nome completo: ')).strip() #strip() elimina os espaços antes e depois
+separa = nome.split() # split() separa o nome em uma lista
 
+print('')
+print('Analisando seu nome...')
 print('')
 print('[Letras Maiusculas:] ', nome.upper()) # letras maiusculas
 print('[Letras Minusculas:] ', nome.lower()) # letras minusculas
-print('[Cumprimento String:]', len(nome), 'letras') # Cumprimento da String
-print('[Comprimento 1° Nome:]')
+print('[Cumprimento String:]', len(nome)-nome.count(' '), 'letras') # Cumprimento da String, count() elimina os espaços entre os nomes
+print('[Comprimento 1° Nome:]',separa[0], ':', len(separa[0]), 'letras')
 print('')
+
+print(separa)
+print('------------------------------')
