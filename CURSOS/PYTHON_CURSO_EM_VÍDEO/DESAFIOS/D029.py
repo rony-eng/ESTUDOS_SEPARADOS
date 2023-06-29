054901e4f03f0725a452c74fb321a42fc1b010fb
Rony-Menezes <134240502+rony-eng@users.noreply.github.com>
2023-06-29 14:00:14 -0300

    Update D029 Python
    Resolução segundo o curso em vídeo do desafio D029 de Python

--- CURSOS/PYTHON_CURSO_EM_VÍDEO/DESAFIOS/D029.py
+++ CURSOS/PYTHON_CURSO_EM_VÍDEO/DESAFIOS/D029.py
@@ -8,6 +8,16 @@ vel = int(input('Insira sua velocidade: '))
 multa = (vel - 80) * 7
 if vel > 80:
     print('Você foi multado no valor de {}R$!'.format(multa))
+else:
+    print('Tenha um bom dia! Dirija com segurança!')
 print('------------------------------')
 
-# DESAFIO CONCLUÍDO, FALTA VER RESOLUÇÃO
+# DESAFIO CONCLUÍDO ----------------------------------------------
+
+# RESOLUÇÃO SEGUNDO CURSO ----------------------------------------
+velocidade = float(input('Qual a velocidade atual do carro? '))
+if velocidade > 80:
+    print('MULTADO! Você excedeu o limite permitido que é de 80km/h')
+    multa = (velocidade - 80) * 7
+    print('Você deve pagar uma multa de R${:.2f}!'.format(multa))
+print('Tenha um bom dia! Dirija com segurança!')
