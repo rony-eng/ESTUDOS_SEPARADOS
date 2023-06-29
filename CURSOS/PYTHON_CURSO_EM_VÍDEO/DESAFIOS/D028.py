d00a5ab71fa65ade6df11515ff1231f87c844e49
Rony-Menezes <134240502+rony-eng@users.noreply.github.com>
2023-06-29 13:33:35 -0300

    Update D028 Python
    Resolução do desafio D028 segundo o curso em vídeo - Python

--- CURSOS/PYTHON_CURSO_EM_VÍDEO/DESAFIOS/D028.py
+++ CURSOS/PYTHON_CURSO_EM_VÍDEO/DESAFIOS/D028.py
@@ -4,6 +4,8 @@
 #O PROGRAMA DEVERÁ ESCREVER NA TELA SE O USUÁRIO VENCEU OU PERDEU.
 
 import random
+from time import sleep
+
 pensar = [0,1,2,3,4,5]
 escolhido = random.choice(pensar)
 num = int(input('Digite um número de 0 a 5: '))
@@ -13,4 +15,17 @@ else:
     print('Número incorreto!')
     print('Numero correto é {}!'.format(escolhido))
 
-# DESAFIO CONCLUÍDO, FALTA VER RESOLUÇÃO
+# DESAFIO CONCLUÍDO ----------------------------------------------
+
+# RESOLUÇÃO ------------------------------------------------------
+computador = random.randiant(0,5) # faz o computador 'pensar'
+print('-=-'*20)
+print('Vou pensar em um número entre 0 e 5. Tente adivinhar...')
+print('-=-'*20)
+jogador = int(input('Em que número eu pensei? '))
+print('PROCESSANDO...')
+sleep(3) # faz o computador "dormir" por 3 segundos.
+if jogador == computador:
+    print('PARABENS! Você conseguiu me vencer')
+else:
+    print('GANHEI! Eu pensei no número {} e não no {}!'.format(computador,jogador))
