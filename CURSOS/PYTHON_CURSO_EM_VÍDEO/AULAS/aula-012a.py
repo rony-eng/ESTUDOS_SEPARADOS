9e1c67f3c0c49d3e5669e95abd2b00aba8bb5074
Rony-Menezes <134240502+rony-eng@users.noreply.github.com>
2023-07-14 14:56:54 -0300

    Update aula 012a
    Anotações da aula 012a de Python do curso em vídeo.

---
+++ CURSOS/PYTHON_CURSO_EM_VÍDEO/AULAS/aula-012a.py
@@ -0,0 +1,13 @@
+# CONDIÇÕES ANINHADAS
+
+# estrutura condicional aninhada
+nome = str(input('Qual é seu nome? '))
+if nome == 'Rony':
+    print('Que nome bonito!')
+elif nome == 'Pedro' or nome == 'Maria' or nome == 'Paulo':
+    print('Seu nome é bem popular no Brasil!')
+elif nome in 'Ana Claudia Jéssica Juliana':
+    print('Belo nome feminino!')
+else:
+    print('Seu nome é bem normal!')
+print('Tenha um bom dia, {}'.format(nome))
