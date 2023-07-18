4e09e573af85885f00a3d1ee4975d4fbc67f7a93
Rony-Menezes <134240502+rony-eng@users.noreply.github.com>
2023-07-18 16:34:46 -0300

    Desafio D037
    Meia resolução do desafio, incompleto.

--- CURSOS/PYTHON_CURSO_EM_VÍDEO/DESAFIOS/D037.py
+++ CURSOS/PYTHON_CURSO_EM_VÍDEO/DESAFIOS/D037.py
@@ -1,2 +1,24 @@
 #ESCREVA UM PROGRAMA QUE LEIA UM NÚMERO INTEIRO QUALQUER E PEÇA PARA O USUÁRIO 
 #ESCOLHER QUAL SERÁ A BASE DE CONVERSÃO: 1=BINÁRIO 2=OCTAL 3=HEXADECIMAL
+
+print('')
+print('----------------------------------------')
+
+num = int(input('Insira um número para a conversão: '))
+base = input('Qual base deseja? [1 = BINÁRIO] [2 = OCTAL] [3 = HEXADECIMAL]: ')
+
+print('----------------------------------------')
+print('')
+
+if base == '1':
+    print('CONVERSÃO BINÁRIA:')
+    print('RESULTADO []')
+    print('')
+elif base == '2':
+    print('CONVERSÃO OCTAL:')
+    print('RESULTADO []')
+    print('')
+else:
+    print('CONVERSÃO HEXADECIMAL:')
+    print('RESULTADO []')
+    print('')
