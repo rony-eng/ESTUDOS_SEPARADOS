695b10ad49a8100ab8a3bb117b5c29be07c97f43
Rony-Menezes <134240502+rony-eng@users.noreply.github.com>
2023-07-19 12:19:17 -0300

    Desafio D038 Concluído
    Resolução do desafio D038 de Python do curso em vídeo, de acordo com o curso.

--- CURSOS/PYTHON_CURSO_EM_VÍDEO/DESAFIOS/D038.py
+++ CURSOS/PYTHON_CURSO_EM_VÍDEO/DESAFIOS/D038.py
@@ -20,3 +20,12 @@ else:
 # DESAFIO CONCLUÍDO
 
 # RESOLUÇÃO DO CURSO
+
+n1 = int(input('Primeiro número: '))
+n2 = int(input('Segundo número: '))
+if n1 > n2:
+    print('O PRIMEIRO valor é maior')
+elif n2 > n1:
+    print('O SEGUNDO valor é maior')
+else:
+    print('Os dois valores são IGUAIS')
