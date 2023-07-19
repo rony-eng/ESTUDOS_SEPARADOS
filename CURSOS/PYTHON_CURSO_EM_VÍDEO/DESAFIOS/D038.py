7b7d52c5db8adff306b236184f1a5e8220eb9edd
Rony-Menezes <134240502+rony-eng@users.noreply.github.com>
2023-07-19 12:14:27 -0300

    Desafio D038 Concluído
    Resolução do desafio D038 do curso em Vídeo, minha resolução!

--- CURSOS/PYTHON_CURSO_EM_VÍDEO/DESAFIOS/D038.py
+++ CURSOS/PYTHON_CURSO_EM_VÍDEO/DESAFIOS/D038.py
@@ -1,2 +1,22 @@
 #ESCREVA UM PROGRAMA QUE LEIA DOIS NÚMEROS INTEIROS E COMPARE-OS, MOSTRANDO NA TELA UMA MENSAGEM: 
 #O PRIMEIRO VALOR É MAIOR, O SEGUNDO VALOR É MAIOR, NÃO EXISTE VALOR MAIOR, OS DOIS SÃO IGUAIS.
+
+print('')
+print('----------------------------------------')
+
+num1 = int(input('Insira o primeiro número: '))
+num2 = int(input('Insira o segundo número: '))
+
+print('----------------------------------------')
+print('')
+
+if num1 > num2:
+    print('O número {} é maior que {}.'.format(num1, num2))
+elif num2 > num1:
+    print('O número {} é maior que {}.'.format(num2, num1))
+else:
+    print('Não existe valor maior, os dois são iguais!')
+
+# DESAFIO CONCLUÍDO
+
+# RESOLUÇÃO DO CURSO
