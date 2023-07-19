59285a6234e165a103a340d3a5e65cb4acbb829f
Rony-Menezes <134240502+rony-eng@users.noreply.github.com>
2023-07-19 11:49:06 -0300

    Desafio D037 Concluído
    Resolução do desafio D037 de Python do curso em Vídeo, resolução minha e do curso

--- CURSOS/PYTHON_CURSO_EM_VÍDEO/DESAFIOS/D037.py
+++ CURSOS/PYTHON_CURSO_EM_VÍDEO/DESAFIOS/D037.py
@@ -4,21 +4,41 @@
 print('')
 print('----------------------------------------')
 
-num = int(input('Insira um número para a conversão: '))
-base = input('Qual base deseja? [1 = BINÁRIO] [2 = OCTAL] [3 = HEXADECIMAL]: ')
+nume = int(input('Insira um número para a conversão: '))
+base = int(input('Qual base deseja? [1 = BINÁRIO] [2 = OCTAL] [3 = HEXADECIMAL]: '))
 
 print('----------------------------------------')
 print('')
 
-if base == '1':
+if base == 1:
     print('CONVERSÃO BINÁRIA:')
-    print('RESULTADO []')
+    print('RESULTADO [{}]'.format(bin(nume)[2:]))
     print('')
-elif base == '2':
+elif base == 2:
     print('CONVERSÃO OCTAL:')
-    print('RESULTADO []')
+    print('RESULTADO [{}]'.format(oct(nume)[2:]))
+    print('')
+elif base == 3:
+    print('CONVERSÃO HEXADECIMAL:')
+    print('RESULTADO [{}]'.format(hex(nume)[2:]))
     print('')
 else:
-    print('CONVERSÃO HEXADECIMAL:')
-    print('RESULTADO []')
-    print('')
+    print('Opção Invalida! Tente Novamente.')
+
+# DESAFIO CONCLUÍDO
+
+# RESOLUÇÃO VERSÃO DO CURSO
+#num = int(input('Digite um número inteiro: '))
+#print('''Escolha uma das bases para conversão:
+#[ 1 ] converter para BINÁRIO
+#[ 2 ] converter para OCTAL
+#[ 3 ] converter para HEXADECIMAL''')
+#opção = int(input('Sua opção: '))
+#if opção == 1:
+#    print('{} convertido para BINÁRIO é igual a {}'.format(num, bin(num)[2:]))
+#elif opção == 2:
+#    print('{} convertido para OCTAL é igual a {}'.format(num, oct(num)[2:]))
+#elif opção == 3:
+#    print('{} convertido para HEXADECIMAL é igual a {}'.format(num, hex(num)[2:]))
+#else:
+#    print('Opção inválida. Tente novamente.')
