ea3fdfa5ba994a98c3497d995105525638c35342
Rony-Menezes <134240502+rony-eng@users.noreply.github.com>
2023-07-21 12:01:16 -0300

    Update Desafio D040

--- CURSOS/PYTHON_CURSO_EM_VÍDEO/DESAFIOS/D040.py
+++ CURSOS/PYTHON_CURSO_EM_VÍDEO/DESAFIOS/D040.py
@@ -1,9 +1,16 @@
 #CRIE UM PROGRAMA QUE LEIA DUAS NOTAS DE UM ALUNO E CALCULE SUA MÉDIA, MOSTRANDO UMA MENSAGEM NO FINAL, 
 #DE ACORDO COM A MÉDIA ATINGIDA: MÉDIA ABAIXO DE 5.0 REPROVADO, MÉDIA ENTRE 5.0 E 6.9 RECUPERAÇÃO, MÉDIA 7.0 OU SUPERIOR APROVADO.
+
+print('')
+print('----------------------------------------')
+
 n1 = float(input('Digite sua primeira nota: '))
 n2 = float(input('Digite sua segunda nota: '))
-media = (n1 + n2) / 2
 
+print('----------------------------------------')
+print('')
+
+media = (n1 + n2) / 2
 if media < 5.0:
     print('[REPROVADO]')
     print('Sua média foi de {:.2f}.'.format(media))
@@ -13,3 +20,9 @@ elif media > 5.0 and media < 6.9:
 else:
     print('[APROVADO]')
     print('Sua média foi de {:.2f}.'.format(media))
+
+print('')
+
+# DESAFIO CONCLUÍDO
+
+# RESOLUÇÃO DE ACORDO COM O CURSO
