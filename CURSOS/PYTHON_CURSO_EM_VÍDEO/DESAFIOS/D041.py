510ede4a61e10b2a01473b571c437c8531c92925
Rony-Menezes <134240502+rony-eng@users.noreply.github.com>
2023-07-22 11:24:25 -0300

    Desafio D041 Concluído
    Minha resolução do desafio D041

--- CURSOS/PYTHON_CURSO_EM_VÍDEO/DESAFIOS/D041.py
+++ CURSOS/PYTHON_CURSO_EM_VÍDEO/DESAFIOS/D041.py
@@ -1,2 +1,34 @@
 #A CONFEDERAÇÃO NACIONAL DE NATAÇÃO PRECISA DE UM PROGRAMA QUE LEIA O ANO DE NASCIMENTO DE UM ATLETA 
 #E MOSTRE SUA CATEGORIA DE ACORDO COM A IDADE: ATÉ 9 ANOS MIRIM, ATÉ 14 ANOS INFANTIL, ATÉ 19 ANOS JUNIOR, ATÉ 20 ANOS SÊNIOR, ACIMA MASTER
+from datetime import date
+print('')
+print('----------------------------------------')
+
+ano_nascimento = int(input('[INFORME SEU ANO DE NASCIMENTO] '))
+ano_atual = date.today().year
+idade = ano_atual - ano_nascimento
+
+print('----------------------------------------')
+print('')
+
+if idade <= 9:
+    print('{} anos'.format(idade))
+    print('[CATEGORIA MIRIN]')
+elif idade <= 14:
+    print('{} anos'.format(idade))
+    print('[CATEGORIA INFANTIL]')
+elif idade <= 19:
+    print('{} anos'.format(idade))
+    print('[CATEGORIA JUNIOR]')
+elif idade > 19 and idade <= 20:
+    print('{} anos'.format(idade))
+    print('[CATEGORIA SÊNIOR]')
+else:
+    print('{} anos'.format(idade))
+    print('[CATEGORIA MASTER]')
+
+print('')
+
+# DESAFIO CONCLUÍDO
+
+# RESOLUÇÃO DE ACORDO COM O CURSO
