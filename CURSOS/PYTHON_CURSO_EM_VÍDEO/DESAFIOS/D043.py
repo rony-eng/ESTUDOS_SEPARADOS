8d7634a1a0368478a0e790c0f0dea1694aeb3e5c
Rony-Menezes <134240502+rony-eng@users.noreply.github.com>
2023-07-24 11:48:40 -0300

    Desafio D043 Concluído
    Resolução do desafio D043 de acordo com o curso em vídeo

--- CURSOS/PYTHON_CURSO_EM_VÍDEO/DESAFIOS/D043.py
+++ CURSOS/PYTHON_CURSO_EM_VÍDEO/DESAFIOS/D043.py
@@ -28,3 +28,18 @@ print('')
 # DESAFIO CONCLUÍDO
 
 # RSOLUÇÃO DE ACORDO COM O CURSO
+
+peso = float(input('Qual é seu peso? (Kg) '))
+altura = float(input('Qual é sua altura? (m) '))
+imc = peso / (altura ** 2)
+print('O IMC dessa pessoa é de {:.1f}'.format(imc))
+if imc < 18.5:
+    print('Você está ABAIXO DO PESO normal')
+elif 18.5 <= imc < 25:
+    print('PARABÉNS, você está na faixa de PESO NORMAL')
+elif 25 <= imc < 30:
+    print('Você está em SOBREPESO')
+elif 30 <= imc < 40:
+    print('Você está em OBESIDADE!')
+elif imc >= 40:
+    print('Você está em OBESIDADE MÓRBIDA, cuidado!')
