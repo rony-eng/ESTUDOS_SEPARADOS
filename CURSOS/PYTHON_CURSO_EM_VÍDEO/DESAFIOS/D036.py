d2d1cd5911225c04e26f5df36c82d4a000809bf8
Rony-Menezes <134240502+rony-eng@users.noreply.github.com>
2023-07-24 18:52:53 -0300

    Update D036

--- CURSOS/PYTHON_CURSO_EM_VÍDEO/DESAFIOS/D036.py
+++ CURSOS/PYTHON_CURSO_EM_VÍDEO/DESAFIOS/D036.py
@@ -27,14 +27,14 @@ else:
 # DESAFIO CONCLUÍDO
 
 # RESOLUÇÃO DE ACORDO COM CURSO EM VÍDEO
-# casa = float(input('Valor da casa: R$'))
-# salário = float(input('Salário do comprador: R$'))
-# anos = int(input('Quantos anos de financiamento? '))
-# prestação = casa / (anos * 12)
-# mínimo = salário * 30 / 100
-# print('Para pagar uma casa de R${:.2f} em {} anos'.format(casa, anos), end='')
-# print(' a prestação será de R${:.2f}'.format(prestação))
-# if prestação <= mínimo:
-#     print('Emprestimo pode ser CONCEDIDO!')
-# else:
-#     print('Emprestimo NEGADO!')
+casa = float(input('Valor da casa: R$'))
+salário = float(input('Salário do comprador: R$'))
+anos = int(input('Quantos anos de financiamento? '))
+prestação = casa / (anos * 12)
+mínimo = salário * 30 / 100
+print('Para pagar uma casa de R${:.2f} em {} anos'.format(casa, anos), end='')
+print(' a prestação será de R${:.2f}'.format(prestação))
+if prestação <= mínimo:
+    print('Emprestimo pode ser CONCEDIDO!')
+else:
+    print('Emprestimo NEGADO!')
