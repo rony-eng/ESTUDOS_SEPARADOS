e3017a0e965a2f8b12ab09f83ab1f2411b8f14a1
Rony-Menezes <134240502+rony-eng@users.noreply.github.com>
2023-07-25 14:47:09 -0300

    Desafio D044 Concluído
    Resolução do desafio D044 de acordo com  o curso em vídeo

--- CURSOS/PYTHON_CURSO_EM_VÍDEO/DESAFIOS/D044.py
+++ CURSOS/PYTHON_CURSO_EM_VÍDEO/DESAFIOS/D044.py
@@ -45,3 +45,26 @@ else:
 # DESAFIO CONCLUÍDO
 
 # RESOLUÇÃO DE ACORDO COM O CURSO
+preço = float(input('Preço das compras: R$ '))
+print(''' Formas de Pagamento
+[1] á vista dinheiro / cheque
+[2] á visa cartão
+[3] 2x no cartão
+[4] 3x ou mais no cartão''')
+opção = int(input('Qual é a opção? '))
+if opção == 1:
+    total = preço - (preço * 10 / 100)
+elif opção == 2:
+    total = preço - (preço * 5 / 100)
+elif opção == 3:
+    total = preço
+    parcela = total / 2
+    print('Sua compra será parcela em 2x de R${:.2f} SEM JUROS'.format(parcela))
+elif opção == 4:
+    total = preço + (preço * 20 / 100)
+    totparc = int(input('Quantas parcelas: '))
+    parcela = total / totparc
+    print('Sua compra será parcelada em {}x de R${:.2f} COM JUROS'.format(totparc, parcela))
+else:
+    print('OPÇÃO INVALIDA!')
+print('Sua compra de R${:.2f} vai custar R${:.2f} no final.'.format(preço, total))
