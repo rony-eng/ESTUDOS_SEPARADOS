588cfeb23132808f3d809eac482f089f24a39911
Rony-Menezes <134240502+rony-eng@users.noreply.github.com>
2023-07-27 16:44:37 -0300

    Update Jogo Jokenpô - Concluído
    Resolução do bug, logica concluída, posteriormente feito um código mais bonito visivelmente.

--- PYTHON/PROJETOS/jogo_jokenpô.py
+++ PYTHON/PROJETOS/jogo_jokenpô.py
@@ -1,59 +1,64 @@
 from random import randint
 from time import sleep
-itens = ('Pedra', 'Papel', 'Tesoura')
-computador = randint(0,2)
 
 print('INICIANDO JOGO')
 print('''Suas opções:
-      [0] PEDRA
-      [1] PAPEL
-      [2] TESOURA''')
+      [1] PEDRA
+      [2] PAPEL
+      [3] TESOURA''')
 jogador = int(input('Qual é a sua jogada? '))
 
-while jogador != 00:
+while jogador != 0:
+    itens = ('Pedra', 'Papel', 'Tesoura')
+    computador = randint(0,2)
     print('-=' * 11)
     print('JO')
     sleep(1)
     print('KEN')
     sleep(1)
     print('PO!!!')
-    print('Jogador jogou {}'.format(itens[jogador]))
+    if jogador == 1:
+        print('Jogador jogou Pedra')
+    elif jogador == 2:
+        print('Jogador jogou Papel')
+    elif jogador == 3:
+        print('Jogador jogou Tesoura')
+
     print('Computador jogou {}'.format(itens[computador]))
     if computador == 0: # computador jogou Pedra
-        if jogador == 0:
+        if jogador == 1:
             print('EMPATE')
-        elif jogador == 1:
-            print('JOGADOR VENCE')
         elif jogador == 2:
+            print('JOGADOR VENCE')
+        elif jogador == 3:
             print('COMPUTADOR VENCE')
         else:
             print('JOGADA INVÁLIDA')
     elif computador == 1: # computador jogou Papel
-        if jogador == 0:
+        if jogador == 1:
             print('COMPUTADOR VENCE')
-        elif jogador == 1:
-            print('EMPATE')
         elif jogador == 2:
+            print('EMPATE')
+        elif jogador == 3:
             print('JOGADOR VENCE')
         else:
             print('JOGADA INVÁLIDA')
     elif computador == 2: # computador jogou Tesoura
-        if jogador == 0:
+        if jogador == 1:
             print('JOGADOR VENCE')
-        elif jogador == 1:
-            print('COMPUTADOR VENCE')
         elif jogador == 2:
+            print('COMPUTADOR VENCE')
+        elif jogador == 3:
             print('EMPATE')
         else:
             print('JOGADA INVÁLIDA')
     print('-=' * 11)
     print('''Suas opções:
-      [0] PEDRA
-      [1] PAPEL
-      [2] TESOURA''')
+      [1] PEDRA
+      [2] PAPEL
+      [3] TESOURA''')
     jogador = int(input('Qual é a sua jogada? '))
     
 
 print('FIM DE JOGO')
-# JOGO INCOMPLETO / FALTA AJEITAR UM ERRO QUANDO COLOCA O NUMERO 0 NO JOGADOR, ELE FECHA O JOGO O QUE NÃO 
-# ERA PRA ACONTECER.
+# JOGO CONCLUÍDO
