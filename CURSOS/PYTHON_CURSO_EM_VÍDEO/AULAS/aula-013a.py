b345a6471469178643091bcc2ab91d2ce6f85bce
Rony-Menezes <134240502+rony-eng@users.noreply.github.com>
2023-07-28 11:18:31 -0300

    Create aula-013a.py
    Criação da aula 013 - notas

---
+++ CURSOS/PYTHON_CURSO_EM_VÍDEO/AULAS/aula-013a.py
@@ -0,0 +1,28 @@
+# ESTRUTUEA DE REPETIÇÃO FOR
+
+# ESTRUTURA, para o boneco chega até a maça e pegar:
+for c in range(1,10):
+    passo
+pega
+
+# ESTRUTURA, para o boneco dá um passo e pular e depois pegar a maça
+for c in range(0,3):
+    passo
+    pula
+passo
+pega
+
+# ESTRUTURA, para o boneco dá um passo, pular, pega moeda e depois pegar a maça
+for c in range(0,3):
+    if tiver moeda:
+        pega
+    passo
+    pula
+passo
+pega
+
+# ------------------------------------------------------------------------------
+# PARTE PRÁTICA
+for c in range(0,6):
+    print('Oi')
+print('FIM')
