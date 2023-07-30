f64c6e3ff43438d7728f05999d8d933e3c3dfb40
Rony-Menezes <134240502+rony-eng@users.noreply.github.com>
2023-07-30 10:54:21 -0300

    Update aula-013a
    Novas atualizações da aula de estrutura for

--- CURSOS/PYTHON_CURSO_EM_VÍDEO/AULAS/aula-013a.py
+++ CURSOS/PYTHON_CURSO_EM_VÍDEO/AULAS/aula-013a.py
@@ -1,25 +1,25 @@
 # ESTRUTUEA DE REPETIÇÃO FOR
 
 # ESTRUTURA, para o boneco chega até a maça e pegar:
-for c in range(1,10):
-    passo
-pega
+#for c in range(1,10):
+#    passo
+#pega
 
 # ESTRUTURA, para o boneco dá um passo e pular e depois pegar a maça
-for c in range(0,3):
-    passo
-    pula
-passo
-pega
+#for c in range(0,3):
+#    passo
+#    pula
+#passo
+#pega
 
 # ESTRUTURA, para o boneco dá um passo, pular, pega moeda e depois pegar a maça
-for c in range(0,3):
-    if tiver moeda:
-        pega
-    passo
-    pula
-passo
-pega
+#for c in range(0,3):
+#    if tiver moeda:
+#        pega
+#    passo
+#    pula
+#passo
+#pega
 
 # ------------------------------------------------------------------------------
 # PARTE PRÁTICA
@@ -27,3 +27,8 @@ for c in range(0,6):
     print('Oi')
     print('FIM')
 print('TERMINO DO FOR')
+
+# PARA A ORDEM AO CONTRÁRIO
+for c in range (6, 0, -1):
+    print(c)
+print('Fim!')
