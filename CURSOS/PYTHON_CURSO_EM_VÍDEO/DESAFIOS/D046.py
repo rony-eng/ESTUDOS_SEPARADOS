e323d5027523791c865c0e6ac8151a81165c6655
Rony-Menezes <134240502+rony-eng@users.noreply.github.com>
2023-08-01 15:59:03 -0300

    Desafio D046 Concluído
    Minha resolução do desafio D046 de python do curso em vídeo

--- CURSOS/PYTHON_CURSO_EM_VÍDEO/DESAFIOS/D046.py
+++ CURSOS/PYTHON_CURSO_EM_VÍDEO/DESAFIOS/D046.py
@@ -1,2 +1,13 @@
 # D046 - FAÇA UM PROGRAMA QUE MOSTRE NA TELA UMA CONTAGEM REGRESSIVA PARA O ESTOURO DE 
 # FOGOS DE ARTIFÍCIO, INDO DE 10 ATÉ 0, COM UMA PAUSA DE 1 SEGUNDO ENTRE ELES.
+from time import sleep
+# sleep(1) para dá um segundo de um processo para o outro
+
+print('Contagem regressiva para o estouro dos FOGOS!')
+sleep(1)
+for i in range(10, 0, -1):
+    print(i)
+    sleep(1)
+print('Feliz Ano Novo!')
+
+# DESAFIO CONCLUÍDO
