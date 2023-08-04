3b157284a6a548b63af923366ed21d335bcbff64
Rony-Menezes <134240502+rony-eng@users.noreply.github.com>
2023-08-04 14:40:51 -0300

    Desafio D048 Concluído
    Resolução do  desafio D048 Python de acordo com o curso em vídeo.

--- CURSOS/PYTHON_CURSO_EM_VÍDEO/DESAFIOS/D048.py
+++ CURSOS/PYTHON_CURSO_EM_VÍDEO/DESAFIOS/D048.py
@@ -1,1 +1,11 @@
 # FAÇA UM PROGRAMA QUE CALCULE A SOMA ENTRE TODOS OS NÚMEROS IMPARES QUE SÃO MÚLTIPLOS DE TRÊS E QUE SE ENCONTRAM NO INTERVALO DE 1 ATÉ 500
+
+
+# RESOLUÇÃO DE ACORDO COM O CURSO
+soma = 0
+cont = 0 
+for c in range (1, 501):
+    if c % 3 == 0:
+        cont = cont + 1
+        soma = soma + c
+print('A soma de todos os {} valores solicitados é {}'.format(cont, soma))
