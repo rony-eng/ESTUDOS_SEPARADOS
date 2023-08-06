ef32ac1e5eb2ca9e60c10335703b2633871c9374
Rony-Menezes <134240502+rony-eng@users.noreply.github.com>
2023-08-06 18:49:27 -0300

    Desafio D049 Concluído
    Resolução do desafio D049 de Python de acordo com o curso.

--- CURSOS/PYTHON_CURSO_EM_VÍDEO/DESAFIOS/D049.py
+++ CURSOS/PYTHON_CURSO_EM_VÍDEO/DESAFIOS/D049.py
@@ -1,6 +1,12 @@
 # REFAÇA O DESAFIO 009, MOSTRANDO A TABUADA DE UM NÚMERO QUE O USUÁRIO ESCOLHER, SÓ QUE AGORA UTILIZANDO UM LAÇO FOR.
-num = int(input('Insira um número para saber sua tabuada: '))
+numero = int(input('Insira um número para saber sua tabuada: '))
 for i in range(1, 11):
-    print('{} x {:2} = {}'.format(num, i, num*i))
+    print('{} x {:2} = {}'.format(numero, i, numero*i))
 
 # DESAFIO CONCLUÍDO
+
+num = int(input('Digite um número para ver sua tabuada: '))
+for c in range(1, 11):
+    print('{} x {:2} = {}'.format(num, c, num*c))
+
+# RESOLUÇÃO DE ACORDO COM O CURSO
