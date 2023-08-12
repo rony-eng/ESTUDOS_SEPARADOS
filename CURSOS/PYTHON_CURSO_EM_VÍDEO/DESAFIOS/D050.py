26b9424eac984d49221497491c09574fa2474f29
Rony-Menezes <134240502+rony-eng@users.noreply.github.com>
2023-08-12 20:14:22 -0300

    Desafio D050 Concluído
    Resolução do desafio D050 de Python de acordo com o curso em vídeo

--- CURSOS/PYTHON_CURSO_EM_VÍDEO/DESAFIOS/D050.py
+++ CURSOS/PYTHON_CURSO_EM_VÍDEO/DESAFIOS/D050.py
@@ -5,3 +5,15 @@ for i in range(1, 7):
     if n % 2 == 0:
         soma = soma + n
 print('A soma dos números pares é {}'.format(soma))
+
+# DESAFIO CONCLUÍDO
+
+# RESOLUÇÃO DE ACORDO COM O CURSO
+soma = 0
+cont = 0
+for c in range(1, 7):
+    num = int(input('Digite o {} valor: '.format(c)))
+    if num % 2 == 0:
+        soma += num
+        cont += 1
+print('Você informou {} números PARES e a soma foi {}'.format(cont, soma))
