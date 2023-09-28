26d4e62bd305a0dc9d14a06eefa477bb099e9251
Rony-Menezes <134240502+rony-eng@users.noreply.github.com>
2023-09-28 10:24:23 -0300

    Resolução Desafio D049
    Resolução do desafio D049 de acordo com o curso em video de python

--- CURSOS/PYTHON_CURSO_EM_VÍDEO/DESAFIOS/D049.py
+++ CURSOS/PYTHON_CURSO_EM_VÍDEO/DESAFIOS/D049.py
@@ -5,8 +5,7 @@ for i in range(1, 11):
 
 # DESAFIO CONCLUÍDO
 
+# RESOLUÇÃO DE ACORDO COM O CURSO
 num = int(input('Digite um número para ver sua tabuada: '))
 for c in range(1, 11):
     print('{} x {:2} = {}'.format(num, c, num*c))
-
-# RESOLUÇÃO DE ACORDO COM O CURSO
