37990d6b18bf6b5360565be43be4ac53560d2c0e
Rony-Menezes <134240502+rony-eng@users.noreply.github.com>
2023-09-29 09:58:42 -0300

    Desafio D051 Concluído
    Resolução do desafio D051 de acordo com o curso em vídeo de Python

--- CURSOS/PYTHON_CURSO_EM_VÍDEO/DESAFIOS/D051.py
+++ CURSOS/PYTHON_CURSO_EM_VÍDEO/DESAFIOS/D051.py
@@ -1,1 +1,9 @@
 # DESENVOLVA UM PROGRAMA QUE LEIA O PRIMEIRO TERMO E A RAZÃO DE UMA PA, NO FINAL, MOSTRE OS 10 PRIMEIROS TERMOS DESSA PROGRESSÃO.
+
+# RESOLUÇÃO DE ACORDO COM O CURSO
+primeiro = int(input('Primeiro termo: '))
+razão = int(input('Razão: '))
+décimo = primeiro + (10 - 1) * razão
+for c in range(primeiro, décimo + razão, razão):
+    print('{} '.format(c), end='-> ')
+print('ACABOU')
