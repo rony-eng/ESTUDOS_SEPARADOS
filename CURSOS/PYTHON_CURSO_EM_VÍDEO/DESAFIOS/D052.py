b071679f640745affa54e15eb3ffebc114870a44
Rony Menezes <134240502+rony-eng@users.noreply.github.com>
2023-10-08 09:09:48 -0300

    Desafio D052 Python
    Resolução do desafio D052 de Python do Curso em Vídeo

--- CURSOS/PYTHON_CURSO_EM_VÍDEO/DESAFIOS/D052.py
+++ CURSOS/PYTHON_CURSO_EM_VÍDEO/DESAFIOS/D052.py
@@ -1,1 +1,17 @@
 # FAÇA UM PROGRAMA QUE LEIA UM NÚMERO INTEIRO E DIGA SE ELE É OU NÃO UM NÚMERO PRIMO.
+
+# RESOLUÇÃO DE ACORDO COM O CURSO
+num = int(input('Digite um número: '))
+tot = 0
+for c in range(1, num + 1):
+    if num % c == 0:
+        print('\033[33m', end='')
+        tot += 1
+    else:
+        print('\033[31m', end='')
+    print('{}'.format(c), end='')
+print('\nO número {} foi divisível {} vezes'.format(num, tot))
+if tot == 2:
+    print('É por isso ele é PRIMO')
+else:
+    print('É por isso ele NÃO É PRIMO')
