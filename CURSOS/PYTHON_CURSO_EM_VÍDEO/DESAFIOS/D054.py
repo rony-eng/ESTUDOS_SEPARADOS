51f43c2473e6163172819166178a2c8a4c4e9d5a
Rony Menezes <134240502+rony-eng@users.noreply.github.com>
2023-10-10 11:00:59 -0300

    Resolução Desafio D054
    Resolução do desafio D054.py de pyhon de acordo com o curso em vídeo

--- CURSOS/PYTHON_CURSO_EM_VÍDEO/DESAFIOS/D054.py
+++ CURSOS/PYTHON_CURSO_EM_VÍDEO/DESAFIOS/D054.py
@@ -1,1 +1,15 @@
 # CRIE UM PROGRAMA QUE LEIA O ANO DE NASCIMENTO DE SETE PESSOAS. NO FINAL, MOSTRE QUANTAS PESSOAS AINDA NÃO ATINGIRAM A MAIORIDADE E QUANTAS JÁ SÃO MAIORES.
+from datetime import date
+atual = date.today().year
+totmaior = 0
+totmenor = 0
+for pess in range(1, 8):
+    nasc = int(input('Em que ano a {}° pessoa nasceu? '.format(pess)))
+    idade = atual - nasc
+    print('Essa pessoa tem {} anos'.format(idade))
+    if idade >= 21:
+        totmaior += 1
+    else:
+        totmenor += 1
+print('Ao todo tivemos {} pessoas maiores de idade'.format(totmaior))
+print('E também tivemos {} pessoas menores de idade'.format(totmenor))
