ae924c0e6357cb9feab10c3ddd4aba81d39d4f8b
Rony Menezes <134240502+rony-eng@users.noreply.github.com>
2023-10-11 11:14:38 -0300

    Resolução Desafio D055
    Resolução do desafio D055 de Python de acordo com o curso em vídeo

--- CURSOS/PYTHON_CURSO_EM_VÍDEO/DESAFIOS/D055.py
+++ CURSOS/PYTHON_CURSO_EM_VÍDEO/DESAFIOS/D055.py
@@ -1,1 +1,15 @@
 # FAÇA UM PROGRAMA QUE LEIA O PESO DE CINCO PESSOAS. NO FINAL, MOSTRE QUAL FOI O MAIOR E O MENOR PESO LIDOS.
+maior = 0
+menor = 0
+for p in range(1,6):
+    peso = float(input('Peso da {}° pessoa: '.format(p)))
+    if p == 1:
+        maior = peso
+        menor = peso
+    else:
+        if peso > maior:
+            maior = peso
+        if peso < menor:
+            menor = peso
+print('O maior peso lido foi de {}kg'.format(maior))
+print('O menor peso lido foi de {}kg'.format(menor))
