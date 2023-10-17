f4eff01e80ca919b40b0b68173758ba4e413a33c
Rony Menezes <134240502+rony-eng@users.noreply.github.com>
2023-10-17 16:43:05 -0300

    Resolução Desafio D057
    Resolução do desafio D057 de python do curso em vídeo, minha resolução

--- CURSOS/PYTHON_CURSO_EM_VÍDEO/DESAFIOS/D057.py
+++ CURSOS/PYTHON_CURSO_EM_VÍDEO/DESAFIOS/D057.py
@@ -1,1 +1,18 @@
-# FAÇA UM PROGRAMA QUE LEIA O SEXO DE UMA PESSOA, MAS SÓ ACEITE OS VALORES ‘M’ OU ‘F’. CASO ESTEJA ERRADO, PEÇA A DIGITAÇÃO NOVAMENTE ATÉ TER UM VALOR CORRETO
+# FAÇA UM PROGRAMA QUE LEIA O SEXO DE UMA PESSOA, MAS SÓ ACEITE OS 
+# VALORES ‘M’ OU ‘F’. CASO ESTEJA ERRADO, PEÇA A DIGITAÇÃO NOVAMENTE 
+# ATÉ TER UM VALOR CORRETO
+sexo = str(input('Qual o seu sexo? [M]/[F] ')).upper()
+
+while sexo != 'SAIR':
+    if sexo == 'M':
+        print('Seu sexo é masculino!')
+        break
+    if sexo == 'F':
+        print('Seu sexo é feminino!')
+        break
+    print('')
+    print('RESPOSTA ERRADA! TENTE NOVAMENTE!')    
+    sexo = str(input('Qual o seu sexo? [M]/[F] ')).upper()
+
+
+# INCOMPLETO
