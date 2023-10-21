7d2d18cc2037eeabeff169203b8f2fd639ac0ba9
Rony Menezes <134240502+rony-eng@users.noreply.github.com>
2023-10-21 10:44:17 -0300

    Update Desafio D057
    Resolução do desafio D057 de python de acordo com o curso em vídeo

--- CURSOS/PYTHON_CURSO_EM_VÍDEO/DESAFIOS/D057.py
+++ CURSOS/PYTHON_CURSO_EM_VÍDEO/DESAFIOS/D057.py
@@ -16,3 +16,10 @@ while sexo != 'SAIR':
 
 
 # RESPOSTA DE ACORDO COM O CURSO
+
+sexo = str(input('Informe seu sexo: [M/F] ')).strip().upper()[0]
+while sexo not in 'MmFf':
+    sexo = str(input('Dados inválidos. Por Favor, informe seu sexo: [M/F] ')).strip().upper()[0]
+print('Sexo {} registrado com sucesso'.format(sexo))
+
+# DESAFIO CONCLUÍDO
