1a895ea857a43491192a1930384e7b86e7e7691a
Rony Menezes <134240502+rony-eng@users.noreply.github.com>
2023-10-23 15:22:13 -0300

    Update Desafio D058 Resolução
    Resolução do Desafio D058 de python de acordo com o curso em vídeo.

--- CURSOS/PYTHON_CURSO_EM_VÍDEO/DESAFIOS/D058.py
+++ CURSOS/PYTHON_CURSO_EM_VÍDEO/DESAFIOS/D058.py
@@ -26,3 +26,25 @@ while jogador != computador:
     jogador = int(input('Em que número eu pensei? '))
 
 print('PARABENS! Você conseguiu me vencer em {} tentativas!'.format(contador))
+
+# RESOLUÇÃO DE ACORDO COM O CURSO
+# OBS: COLOCANDO SE O JOGADOR ESTAR MAIS PERTO OU LONGE, COM O
+# COMPUTADOR DIZENDO SE ESTAR MAIS PERTO OU MENOS.
+
+from random import randint
+computador = randint(0, 10)
+print('Sou seu computador... Acabei de pensar em um número entre 0 e 10.')
+print('Será que você consegue adivinhar qual foi? ')
+acertou = False
+palpites = 0
+while not acertou:
+    jogador = int(input('Qual é seu palpite? '))
+    palpites += 1
+    if jogador == computador:
+        acertou = True
+    else:
+        if jogador < computador:
+            print('Mais... Tente mais uma vez.')
+        elif jogador > computador:
+            print('Menos... Tente mais uma vez.')
+print('Acertou com {} tentativas!'.format(palpites))
