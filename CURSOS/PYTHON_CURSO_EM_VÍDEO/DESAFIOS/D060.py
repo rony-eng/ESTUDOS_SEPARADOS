14538d6e0a639a25f5c0fc2296502e2690d801be
Rony Menezes <134240502+rony-eng@users.noreply.github.com>
2023-10-25 18:50:21 -0300

    Update Desafio D060

--- CURSOS/PYTHON_CURSO_EM_VÍDEO/DESAFIOS/D060.py
+++ CURSOS/PYTHON_CURSO_EM_VÍDEO/DESAFIOS/D060.py
@@ -1,2 +1,22 @@
 # FAÇA UM PROGRAMA QUE LEIA UM NÚMERO QUALQUER E MOSTRE O SEU FATORIAL.
 # EX: 5° = 5x4x3x2x1 = 120
+
+# from math import factorial
+# num = int(input('Insira um número e descubra seu fatorial: '))
+# f = factorial(num)
+# print('O fatorial de {} é {}'.format(num, f))
+
+# RESOLUÇÃO DE ACORDO COM O CURSO
+num = int(input('Insira um número e descubra seu fatorial: '))
+c = num
+f = 1 
+print('Calculando {}!'.format(num), end='')
+while c > 0:
+    print('{}'.format(c), end='')
+    print(' x ' if c > 1 else ' = ', end='')
+    f *= c
+    c -= 1
+print('{}'.format(f))
+
+# DESAFIO CONCLUÍDO
+# DESAFIO PROPOSTO, FAZER O MESMO DESAFIO COM O FOR
