2f3782b04321957d09358627f27894ed707774e2
Rony Menezes <134240502+rony-eng@users.noreply.github.com>
2023-10-28 13:48:49 -0300

    Update Desafio D061
    Resolução do desafio D061 de Python de acordo com o curso em vídeo.

--- CURSOS/PYTHON_CURSO_EM_VÍDEO/DESAFIOS/D061.py
+++ CURSOS/PYTHON_CURSO_EM_VÍDEO/DESAFIOS/D061.py
@@ -1,2 +1,12 @@
 # D061 REFAÇA O DESAFIO 051 LENDO O PRIMEIRO TERMO E A RAZÃO DE UMA PA,
 # MOSTRANDO OS 10 PRIMEIROS TERMOS DA PROGRESSÃO USANDO A ESTRUTURA WHILE.
+print('')
+primeiro = int(input('Primeiro termo: '))
+razão = int(input('Razão da PA: '))
+termo = primeiro
+cont = 1
+while cont <= 10:
+    print('{} -> '.format(termo), end='')
+    termo = termo + razão
+    cont += 1
+print('FIM')
