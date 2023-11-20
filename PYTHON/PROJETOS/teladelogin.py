af960dc33bd2349a6e62636882e2162671b2c095
Rony Menezes <134240502+rony-eng@users.noreply.github.com>
2023-11-20 09:51:19 -0300

    Update teladelogin.py

--- PYTHON/PROJETOS/teladelogin.py
+++ PYTHON/PROJETOS/teladelogin.py
@@ -1,12 +1,17 @@
-import tkinter
+#import tkinter
+#
+#janela = tkinter.Tk() # criando a janela
+#janela.geometry('500x300') # definindo o tamanho da janela
 
-janela = tkinter.Tk()
-janela.geometry('500x300')
+#def clique():
+#    print('Fazer Login')
 
-texto = tkinter.Label(janela, text="Fazer Login")
-texto.pack(padx=10, pady=10)
 
-botao = tkinter.Button(janela, text="Login")
-botao.pack(padx=10, pady=10)
+#texto = tkinter.Label(janela, text="Fazer Login") # criando um texto na janela
+#texto.pack(padx=10, pady=10) # definindo o tamanho do texto
 
-janela.mainloop()
+#botao = tkinter.Button(janela, text="Login", command=clique) # criando um botão, para criar um função cria-se lá em cima e passa o command
+#botao.pack(padx=10, pady=10)  # definindo o tamanho do botão
+
+#janela.mainloop()
+
