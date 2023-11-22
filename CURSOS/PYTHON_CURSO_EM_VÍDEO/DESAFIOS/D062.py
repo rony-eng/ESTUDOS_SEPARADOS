ccbfd9d6f795860593dd1c1b7cd55a9711962e24
Rony Menezes <134240502+rony-eng@users.noreply.github.com>
2023-11-22 11:44:01 -0300

    Update Desafio D065.py

--- CURSOS/PYTHON_CURSO_EM_VÍDEO/DESAFIOS/D062.py
+++ CURSOS/PYTHON_CURSO_EM_VÍDEO/DESAFIOS/D062.py
@@ -34,3 +34,22 @@ while novo_termo not in 'Nn':
 print('FIM DO PROGRAMA!')
 
 # DESAFIO CONCLUÍDO
+
+# RESOLUÇÃO DE ACORDO COM CURSO
+print('Gerador de PA')
+print('-=' ** 10)
+primeiro = int(input('Primeiro termo: '))
+razão = int(input('Razão da PA: '))
+termo = primeiro
+cont = 1
+total = 0
+mias = 10
+while mais != 0:
+    total = total + mais
+    while cont <= 10:
+        print('{} -> '.format(termo), end='')
+        termo += razão
+        cont += 1
+    print('PAUSA')
+    mais = int(input('Quantos termos você quer mostrarr a mais? '))
+print('Progressão finalizada com {} termos mostrados.'.format(total))
