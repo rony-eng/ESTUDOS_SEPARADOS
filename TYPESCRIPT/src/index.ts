2a8a01987edd8c975ac0028cc602cc2b728a83d5
Rony Menezes <134240502+rony-eng@users.noreply.github.com>
2023-11-30 08:56:03 -0300

    Create index.ts

---
+++ TYPESCRIPT/src/index.ts
@@ -0,0 +1,10 @@
+// string, boolean, number, ...
+
+const nome:string = 'Rony'
+const idade:number = 18
+
+idade >= 18 ? alert('Maior de idade') : alert('Menor de idade') //operador ternario, substituindo o if e else
+
+const message:string = 'Olá meu nome é ${nome}'; //  apenas alguns testes, náo está ok ainda
+console.log('Olá,  meu nome é ${}')
+console.log(idade);
