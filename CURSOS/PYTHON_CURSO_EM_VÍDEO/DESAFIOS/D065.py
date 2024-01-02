9b3c63ad07c4572f153ece1db3b2ec9724c4b8f3
Rony Menezes <134240502+rony-eng@users.noreply.github.com>
2024-01-02 15:34:47 -0300

    Update D065

--- CURSOS/PYTHON_CURSO_EM_VÍDEO/DESAFIOS/D065.py
+++ CURSOS/PYTHON_CURSO_EM_VÍDEO/DESAFIOS/D065.py
@@ -24,13 +24,21 @@ print('O maior valor digitado foi {}'.format(maior))
 
 # resolução de acordo com o curso
 resp = 'S'
-soma = quant = média = 0
+soma = quant = média = maior = menor = 0
 while resp in 'Ss':
     núm = int(input('Digite um número: '))
     soma += núm
     quant += 1
+    if quant == 1:
+        maior = menor = núm
+    else:
+        if núm > maior:
+            maior = núm
+        if núm < menor:
+            menor = núm
 
     resp = str(input('Quer continuar? [S/N] ')).upper().strip()[0]
 média = soma / quant
 print('Você digitou {} números e a média foi {}',format(quant, média))
+print('O maior valor foi {} e o menor foi {}'.format(maior, menor))
 print('Acabou')
