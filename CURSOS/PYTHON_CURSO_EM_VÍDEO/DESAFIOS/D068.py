94e106de8ebcbba1fa45e7f79c39f2d17fee77a6
Rony Menezes <134240502+rony-eng@users.noreply.github.com>
2024-01-17 17:53:51 -0300

    Create D068.py

---
+++ CURSOS/PYTHON_CURSO_EM_VÍDEO/DESAFIOS/D068.py
@@ -0,0 +1,2 @@
+#Faça um programa que jogue par ou impar com o computador. O jogo só será interrompido quando o jogador perder.
+#Mostrando o total de vitórias consecutivas que ele conquistou no final do jogo.
