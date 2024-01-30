a83d1c3e1f34189ad739042db138dcb840fe02d2
Rony Menezes <134240502+rony-eng@users.noreply.github.com>
2024-01-30 14:20:40 -0300

    Update D067.py

--- CURSOS/PYTHON_CURSO_EM_VÍDEO/DESAFIOS/D067.py
+++ CURSOS/PYTHON_CURSO_EM_VÍDEO/DESAFIOS/D067.py
@@ -15,5 +15,13 @@ print('Fim do programa')
 #Desafio concluído
 
 #Resolução de acordo com o curso em vídeo
-
+while True:
+    n = int(input('Quer ver a tabuada de qual valor? '))
+    if n < 0:
+        break
+    print('-' * 30)
+    for c in range(1, 11):
+        print(f'{n} x {c} = {n*c}')
+    print('-' * 30)
+print('PROGRAMA TABUADA ENCERRADO. Volte Sempre!')
 
