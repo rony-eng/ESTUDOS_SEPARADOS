e54cda498160d3b03cf5d09b71512f65af5b2c30
Rony Menezes <134240502+rony-eng@users.noreply.github.com>
2024-01-30 14:22:02 -0300

    Update D067.py

--- CURSOS/PYTHON_CURSO_EM_VÍDEO/DESAFIOS/D067.py
+++ CURSOS/PYTHON_CURSO_EM_VÍDEO/DESAFIOS/D067.py
@@ -17,9 +17,9 @@ print('Fim do programa')
 #Resolução de acordo com o curso em vídeo
 while True:
     n = int(input('Quer ver a tabuada de qual valor? '))
+    print('-' * 30)
     if n < 0:
         break
-    print('-' * 30)
     for c in range(1, 11):
         print(f'{n} x {c} = {n*c}')
     print('-' * 30)
