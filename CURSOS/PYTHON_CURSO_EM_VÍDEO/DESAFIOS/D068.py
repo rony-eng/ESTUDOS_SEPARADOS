f9dcb9d22dc0ddbce5f9ccc9f89d9079524e2cbb
Rony Menezes <134240502+rony-eng@users.noreply.github.com>
2024-01-31 18:56:12 -0300

    Update D068.py

--- CURSOS/PYTHON_CURSO_EM_VÍDEO/DESAFIOS/D068.py
+++ CURSOS/PYTHON_CURSO_EM_VÍDEO/DESAFIOS/D068.py
@@ -1,2 +1,11 @@
 #Faça um programa que jogue par ou impar com o computador. O jogo só será interrompido quando o jogador perder.
 #Mostrando o total de vitórias consecutivas que ele conquistou no final do jogo.
+
+# MINHA RESOLUÇÃO
+print('')
+jogador = int(input('Sua escolha: 1[PAR] 2[INPAR]: '))
+if jogador == 1:
+    print('Sua escolha foi PAR!')
+else:
+    print('Sua escolha foi IMPAR!')
+print('')
