8f46e7a8658e208a07b62f4859d263105cff6bde
Rony Menezes <134240502+rony-eng@users.noreply.github.com>
2024-02-23 12:18:41 -0300

    Update D070.py

--- CURSOS/PYTHON_CURSO_EM_VÍDEO/DESAFIOS/D070.py
+++ CURSOS/PYTHON_CURSO_EM_VÍDEO/DESAFIOS/D070.py
@@ -4,33 +4,36 @@
 #    B) Quantos produtos custam mais de 1000
 #    C) Qual é o nome do produto mais barato
 
-#totG = pM1000 = pMB = 0
-#while True:
-#    produto = str(input('Produto: '))
-#    preço = int(input('Valor: '))
-#    if 
-#    resposta = ' '
-#    while resposta not in "SN":
-#        resposta = str(input('Deseja adicionar outro produto? ')).strip().upper()[0]
-#    if resposta == 'N':
-#        break
-
-
-
 # Programa feito pelo copilot
 # Lista para armazenar os produtos
 produtos = []
+totG = pM1000 = pMB = 0
+nome_produto_MB = ''
 
 while True:
     # Lê o nome e o preço do produto
-    nome = input("Digite o nome do produto: ")
+    nome = str(input("Digite o nome do produto: "))
     preco = float(input("Digite o preço do produto: R$"))
 
     # Adiciona o produto à lista
     produtos.append({"nome": nome, "preco": preco})
 
+    # total gasto das compras
+    if preco != 0:
+        totG += preco
+    
+    # Quantos produtos custam mais de 1000
+    if preco > 1000:
+        pM1000 += 1
+    
+    # Qual o nome do produto mais barato
+    pMB = preco
+    if preco < pMB: # está parte está errada
+        nome_produto_MB = nome
+
     # Pergunta se o usuário quer continuar
     continuar = input("Deseja adicionar mais produtos? (S/N): ").strip().lower()
+    print('')
     if continuar != "s":
         break
 
@@ -38,3 +41,7 @@ while True:
 print("\nProdutos adicionados:")
 for produto in produtos:
     print(f"{produto['nome']} - R${produto['preco']:.2f}")
+print('')
+print(f'Total gasto: R${totG}')
+print(f'{pM1000} produtos custam mais de 1000 reais')
+print(f'O produto mais barato foi {nome_produto_MB}')
