8178b363a21caadafd542e4aabd75ff55fa6e481
Rony Menezes <134240502+rony-eng@users.noreply.github.com>
2024-03-03 11:57:28 -0300

    Create D071.py

---
+++ CURSOS/PYTHON_CURSO_EM_VÍDEO/DESAFIOS/D071.py
@@ -0,0 +1,45 @@
+#Crie um programa que simule o funcionamento de um caixa eletrônico. 
+#No inicio, pergunte ao usuário qual será o valor a ser sacado (número inteiro) e o 
+#programa vai informar quantas cédulas de cada valor serão entregues..
+#obs: Considere que o caixa possui cedulas de 50, 20, 10 e 1.
+
+print('=' * 30)
+print("{:^30}".format("CAIXA ELETRÔNICO"))
+print('=' * 30)
+
+valor_do_saque = int(input('Digite o valor do saque: R$'))
+total = valor_do_saque
+
+print('\n\nAnalisando a transação....')
+print('-[ Verificando disponibilidade de cédulas ]-')
+
+cedula = 100
+total_de_cedulas = 0
+
+while True:
+    if valor_do_saque >= cedula:
+        valor_do_saque -= cedula
+        total_de_cedulas += 1
+    else:
+        if total_de_cedulas > 0:
+            print(f"Total de {total_de_cedulas} cédulas de R${cedula}...[OK]")
+        if cedula == 100:
+            cedula = 50
+        elif cedula == 50:
+            cedula = 20
+        elif cedula == 20:
+            cedula = 10
+        elif cedula == 10:
+            cedula = 5
+        elif cedula == 5:
+            cedula = 2
+        total_de_cedulas = 0
+
+    if valor_do_saque == 0:
+        print('=' * 40)
+        print(f"Transação realizada com sucesso!\nSaque no valor total de => : R${total}")
+        exit()
+    elif valor_do_saque == 1:
+        print("\nOPERAÇÃO CANCELADA!\nAs cédulas disponíveis neste caixa não permitem concluir esta transação."
+              "\nPor favor, refaça sua operação com outro valor!")
+        exit()
